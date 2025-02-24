135fdb847a3db43d7881c1de3769e23e1fa54a7d
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-02-24 17:24:09 +0530

    enabled cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
  
-app.use(cors()); 
+app.use(cors({ origin: "*" })); 
 app.use(express.json());
 const mongoURI = process.env.MONGO_URI; 
 // Connect to MongoDB
